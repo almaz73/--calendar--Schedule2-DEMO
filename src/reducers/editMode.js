ac61792f5fe7b51bc30772fe411fe5bb7a907f3d
almaz73 <almaz_73@mail.ru>
2017-03-03 02:06:54 +0300

    скрол

--- src/reducers/editMode.js
+++ src/reducers/editMode.js
@@ -3,7 +3,6 @@
 */
 export default function myReducer(state=false, action) {
 	if (action.type === "TOGGLE_EDIT") {
-		console.log(" action.mode = ", action.mode);
 		return action.mode !== undefined ? action.mode : !state
 	}
 	return state;
